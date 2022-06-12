26975afffaaa41c2580442713254de7b7e975f79
Bryce Wong <epic2004bmw@gmail.com>
2022-06-11 20:17:18 -0700

    Guides

--- src/routes/guides/__layout-guides.svelte
+++ src/routes/guides/__layout-guides.svelte
@@ -4,12 +4,14 @@
 </script>
 
 <header>
-    <Title title="Ninja Gaming Guides" />
-    <div class="links">
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/guides">Guides</a>
-    </div>
+    <nav>
+        <Title title="Ninja Gaming Guides" />
+        <div class="links">
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/guides">Guides</a>
+        </div> 
+    </nav>
 </header>
 <main>
     <slot />
@@ -20,8 +22,8 @@
 
 <style>
     header {
-        display: flex;
-        justify-content: center;
+        text-align: center;
+        background: rgba(0, 0, 0, 0.1)
     }
     main {
         max-width: 960px;
@@ -35,5 +37,11 @@
     }
     a {
         margin-left: 10px;
+    }
+    nav {
+        display: flex;
+        align-items: center;
+        max-width: 960px;
+        margin: 0 auto;
     }
 </style>
